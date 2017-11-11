85382e976fa2b377d0559f8b74eeadc79b6ecfd1
Sanjay <sanjaykv7@gmail.com>
2017-11-11 10:34:19 +0530

    Fixing

---
+++ src/Hello.java
@@ -0,0 +1,29 @@
+public class Hello {
+    public static void main(String args[]){
+
+      /*  System.out.println("Hello World");
+        int tempInt = 10;
+        byte tempByte = 20;
+        short tempShort = 30;
+        long tempLong = ((5000+10)*(tempByte+tempInt+tempShort));
+        float tempFloat = 20.5f;*/
+
+        /*double kiloGram = 0.45359237;
+        int pound = 10;
+        double poundToKg = pound * kiloGram;
+        System.out.println(poundToKg);*/
+
+       /* char kannadaAh = '\u0C85';
+        System.out.println(kannadaAh);*/
+
+       double doubleOne = 20 ;
+       double doubleTwo = 80 ;
+       double result = doubleOne+doubleTwo*25;
+       double remainder = result%40;
+       if(remainder<=20){
+           System.out.println("total was over the limit");
+       }
+
+
+    }
+}
